cdaf168dcbe549bf3fc27f6b0ac90e49f941699d
YuyangJiang <1120230464@bit.edu.cn>
2026-08-08 19:11:56 +0800

    step33

---
+++ steps/step33.py
@@ -0,0 +1,21 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from dezero import *
+import numpy as np
+
+def f(x) -> Variable:
+    return x ** 4 - 2 * x ** 2
+
+def main():
+    x = Variable(2)
+    y = f(x)
+    y.backward(create_graph=True)
+    print(x.grad)
+    gx = x.grad
+    x.cleargrad()
+    gx.backward()
+    print(x.grad)
+
+
+if __name__ == "__main__":
+    main()
